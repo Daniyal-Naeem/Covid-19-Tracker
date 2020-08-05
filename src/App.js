0aa4b59c2405cb42d16c11afd18b74b56bd8acbf
Daniyal <daniyalnaeem5@gmail.com>
2020-08-05 16:25:05 +0500

    Material UI added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {FormControl, MenuItem, Select} from "@material-ui/core";
 
 function App() {
+ 
+const [countries, setCountries] = useState([
+  "UK", "USA", "Pak"
+]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+ 
+    <div className="app__header">
+      <h1> Covid 19 Tracker</h1>
+
+       <FormControl className="app__dropdown">
+      <Select
+      variant="outlined"
+      value="abc"
+
+      >
+        {
+          countries.map( (country) => (
+          <MenuItem value={country}>{country}</MenuItem>
+          ))
+        }
+         <MenuItem value="worldwide">Worldwide</MenuItem>
+         <MenuItem value="worldwide">Option 1</MenuItem>
+         <MenuItem value="worldwide">Option 2</MenuItem>
+         <MenuItem value="worldwide">Option 3</MenuItem>
+
+        </Select>
+
+    </FormControl>
+
+ </div>
+
+  
+
+
+    {/* header*/}
+    {/* title*/}
+
+    {/* infoboxes*/}
+    {/* infoboxes*/}
+    {/* infoboxes*/}
+
+    {/* table*/}
+    {/* graph*/}
+
+    {/* map*/}
+
     </div>
   );
 }
