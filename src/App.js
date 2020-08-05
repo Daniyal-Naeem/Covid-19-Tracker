082ea86de6df8db564ba8f074e6a5b31e4d3ffd7
Daniyal <daniyalnaeem5@gmail.com>
2020-08-05 20:08:44 +0500

    infoBox & Map & Left/Right

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
 import './App.css';
-import {FormControl, MenuItem, Select} from "@material-ui/core";
+import {FormControl, MenuItem, Select, Card, CardContent} from "@material-ui/core";
 import { useEffect } from 'react';
-
+import InfoBox from "./InfoBox";
+import Map from "./Map";
 
 // https://disease.sh/v3/covid-19/countries
 // State: How to write variable in REACT
@@ -19,7 +20,7 @@ function App() {
       await fetch("https://disease.sh/v3/covid-19/countries")
       .then( (response) => response.json())
       .then((data) => {
- 
+
         const countries = data.map(
           (country) => ({
 
@@ -35,29 +36,33 @@ function App() {
   }, []);
 
 const [countries, setCountries] = useState([]);
+const [country, setCountry] = useState(["Worldwide"]);
+
+const onCountryChange = async (event) => {
+
+  const countryCode = event.target.value;
+  console.log("yes" , countryCode);
+
+  setCountry(countryCode);
+}
 
   return (
     <div className="app">
 
- 
+<div className="app__left">
     <div className="app__header">
       <h1> Covid 19 Tracker</h1>
 
        <FormControl className="app__dropdown">
       <Select
-      variant="outlined"
-      value="abc"
-
-      >
+      variant="outlined" onChange={onCountryChange} value={country}>
+         <MenuItem value="Worldwide">Worldwide</MenuItem>
         {
           countries.map( (country) => (
           <MenuItem value={country.value}>{country.name}</MenuItem>
           ))
         }
-         <MenuItem value="worldwide">Worldwide</MenuItem>
-         <MenuItem value="worldwide">Option 1</MenuItem>
-         <MenuItem value="worldwide">Option 2</MenuItem>
-         <MenuItem value="worldwide">Option 3</MenuItem>
+      
 
         </Select>
 
@@ -65,20 +70,26 @@ const [countries, setCountries] = useState([]);
 
  </div>
 
-  
+ {/* InfoBoxe */}
+ <div className="app__stats">
 
+   <InfoBox title="CoronaVirus Cases" cases={1200} total={20000}/>
+   <InfoBox title="Recovered" cases={1200} total={2000}/>
+   <InfoBox title="Deaths" cases={1200} total={200}/>
+</div>
 
-    {/* header*/}
-    {/* title*/}
-
-    {/* infoboxes*/}
-    {/* infoboxes*/}
-    {/* infoboxes*/}
-
+   {/* Map */}
+    <Map />
+</div>
+    
+<Card className="app__right">
+  <CardContent>
     {/* table*/}
+    <h3>This is the table</h3>
     {/* graph*/}
-
-    {/* map*/}
+    <h3>This is the Graph</h3>
+    </CardContent>
+  </Card>
 
     </div>
   );
---
+++ src/InfoBox.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core'
+
+
+function InfoBox({title, cases, total}) {
+    return (
+      <Card className="info__InfoBox">
+
+<CardContent>
+    <Typography className="info__title" color="textSecondary">
+        {title}
+    </Typography>
+
+    <h2 className="info__cases"> {cases}</h2>
+
+    <Typography className="info__total" color="textSecondary">
+        {total} Total
+    </Typography>
+    </CardContent>
+    </Card>
+    )
+}
+
+export default InfoBox
